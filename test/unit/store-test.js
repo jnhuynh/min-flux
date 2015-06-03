3d2d2a20d486508a3bf24538dd1fb1d2209427e1
John Huynh <john@huynh.io>
2015-06-03 11:38:55 -0400

    Add initial failing dummy test.

---
+++ test/unit/store-test.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var MinFlux = require('../../index');
+
+var expect = require('chai').expect;
+
+describe('ActionCreator', () => {
+  describe('create', () => {
+    it('passes', () => {
+      expect(true).to.equal(false);
+    });
+  });
+});
+
